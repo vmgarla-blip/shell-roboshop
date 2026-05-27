23aa3955ae015f23c3975740b3443c1242eed35e
vmgarla-blip <vmgarla@gmail.com>
2026-05-27 04:52:02 -0500

    hello world script created

--- 09-shipping.sh
+++ 09-shipping.sh
@@ -6,7 +6,7 @@ sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
 sudo chmod -R 755 $LOGS_FOLDER
 LOGS_FILE="$LOGS_FOLDER/$0.log"
 SCRIPT_DIR=$PWD
-MYSQL_HOST=mysql.daws90s.shop
+MYSQL_HOST=mysql.devops90s.online
 
 USERID=$(id -u)
 R="\e[31m"
